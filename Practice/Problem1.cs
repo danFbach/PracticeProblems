57ed57d288e11df0fba81644abc11cfb562907c5
danFbach <quiktekrepair@gmail.com>
2016-03-21 11:32:13 -0500

    problem 1 done.

---
+++ Practice/Problem1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Problem1
+    {
+        public int countTotal;                          
+
+        public int stringCount(string InputString)
+        {                                   
+
+            foreach (char c in InputString)
+            {
+                countTotal++;
+            }
+
+            return countTotal;
+        }
+
+
+        //List<string> InputString = new List<string>();
+        //public int ListCount(List<String> InputString)
+        //{                                           
+        //    foreach(string s in InputString)
+        //    {
+        //        countTotal++;
+        //    }
+        //    return countTotal;
+        //}
+
+          
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputString = Console.ReadLine();
+            Problem1 prob1 = new Problem1();
+            int total = prob1.stringCount(InputString);
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
