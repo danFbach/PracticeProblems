4bb2c4979c3d5b09148cdafca8e17b7b95a5b3fc
danFbach <quiktekrepair@gmail.com>
2016-03-21 12:29:53 -0500

    problem 4 almost done

--- Practice/Problem4.cs
+++ Practice/Problem4.cs
@@ -8,5 +8,38 @@ namespace Practice
 {
     class Problem4
     {
+        public List<int> LongList;
+
+        
+
+        public void RotateToRight()
+        {
+            LongList = new List<int>();
+            LongList.Add(1);
+            LongList.Add(2);
+            LongList.Add(3);
+            LongList.Add(4);
+            LongList.Add(5);
+            LongList.Add(6);
+            LongList.Add(7);
+            LongList.Add(8);
+            LongList.Add(9);
+
+            int listLength = LongList.Count;
+
+            LongList.Insert(0, listLength);
+            LongList.RemoveAt(listLength);
+
+            LongList.Insert(0, listLength);
+            LongList.RemoveAt(listLength);
+
+            for(int i = 0;i < listLength;i++)
+            {
+                LongList[i] += 5;
+            }
+
+        }
+
+
     }
 }
